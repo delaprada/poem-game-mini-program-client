b3fc66e2450312ba0d1290b815c2c5120249a2d1
delaprada <924265424zzr@sina.com>
2021-03-25 10:50:29 +0800

    feat: change baseUrl

--- src/servers/baseUrl.ts
+++ src/servers/baseUrl.ts
@@ -1,7 +1,6 @@
 // 设置不同环境不同路径请求的url地址
 const getBaseUrl = (url: string) => {
-  // let BASE_URL = 'http://127.0.0.1:7001';
-  let BASE_URL = 'https://www.miniprogram.ltd';
+  let BASE_URL = 'http://127.0.0.1:7001';
   if (process.env.NODE_ENV === 'development') {
     //开发环境 - 根据请求不同返回不同的BASE_URL
     console.log('开发模式')
